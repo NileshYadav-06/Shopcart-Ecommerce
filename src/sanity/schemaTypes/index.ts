5e933c9d3169e29c85e76bfd6cad67df4255402d
Nilesh Kumar Yadav <nilesh4149yaduvanshi@gmail.com>
2025-09-27 13:15:42 +0530

    sanity schemaTypes

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,5 +1,24 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from "sanity";
+import { categoryType } from "./categoryType";
+import { blockContentType } from "./blockContentType";
+import { blogCategoryType } from "./blogCategoryType";
+import { addressType } from "./addressType";
+import { orderType } from "./orderType";
+import { brandType } from "./brandType";
+import { authorType } from "./authorType";
+import { productType } from "./productType";
+import { blogType } from "./blogType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
+  types: [
+    categoryType,
+    blockContentType,
+    blogCategoryType,
+    productType,
+    orderType,
+    brandType,
+    blogType,
+    authorType,
+    addressType,
+  ],
+};
